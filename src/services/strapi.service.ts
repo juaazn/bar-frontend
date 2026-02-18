9d88b00f7cb4e27c39dfa413a1c386f45c944acf
Chuans_ <90198371+juaazn@users.noreply.github.com>
2026-02-18 13:20:32 +0100

    added try cath

--- src/services/strapi.service.ts
+++ src/services/strapi.service.ts
@@ -1,7 +1,12 @@
 export async function fetchStrapiData<T>(endpoint: string): Promise<T> {
-  const response = await fetch(`${import.meta.env.STRAPI_URL}/api${endpoint}`);
-  if (!response.ok) {
-    throw new Error(`Failed to fetch data from Strapi: ${response.statusText}`);
+  try {
+    const response = await fetch(`${import.meta.env.STRAPI_URL}/api${endpoint}`);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data from Strapi: ${response.statusText}`);
+    }
+    return response.json();
+  } catch (error) {
+    console.error(error);
+    throw error;
   }
-  return response.json();
 }
